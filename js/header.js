ce4c1340606f9ce4b7249bd7e3b0eb43b29aeaef
tweimann <t@tweimann.xyz>
2021-12-12 22:05:38 +0100

    changed footer and header

--- js/header.js
+++ js/header.js
@@ -1,9 +1,9 @@
 const path = window.location.pathname;
-var formattedPath = path.replace('/', ' / ').replace('.html', '');
+var formattedPath = path.replace('/', ' / ').substring(0, path.lastIndexOf(".html") + 2).replace('index', 'home');
 
 document.write('\
 \
 <header>\
-    <a class="header-text" href="index.html"> tweimann' + formattedPath + '</a>\
+    <a class="header-text" href="index.html">tweimann' + formattedPath + '</a>\
 </header>'
 );
