0af3b73c05d7e9248667c0ad41a8199edd9655d8
tweimann <t@tweimann.xyz>
2021-12-12 22:49:04 +0100

    lol

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 const path = window.location.pathname;
 var formattedPath = path.replace('/', ' / ').substring(0, path.lastIndexOf(".html") + 2).replace('index', 'home');
-if (window.location.pathname = '/') {var formattedPath = ' / home'};
+if (window.location.pathname === '/') {var formattedPath = ' / home'};
 
 document.write('\
 \
