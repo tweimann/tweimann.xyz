f95ea209e909bca1c41a95df9ad8b085de307444
tweimann <t@tweimann.xyz>
2021-12-13 22:15:09 +0100

    commit #420

--- js/footer.js
+++ js/footer.js
@@ -1,12 +1,12 @@
 document.write('\
 <footer>\
     <p class="socials-left">\
-        <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src="./icons/instagram.png" class="socials-icon"></img> @tweimann</a><br>\
-        <a target="_blank" rel="noopener noreferrer" href="mailto:contact@tweimann.xyz"><img src="./icons/mail.png" class="socials-icon"></img> contact@tweimann.xyz</a><br>\
+        <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src="https://content.tweimann.xyz/icons/instagram.png" class="socials-icon"></img> @tweimann</a><br>\
+        <a target="_blank" rel="noopener noreferrer" href="mailto:contact@tweimann.xyz"><img src="https://content.tweimann.xyz/icons/mail.png" class="socials-icon"></img> contact@tweimann.xyz</a><br>\
     </p>\
     <p class="socials-right">\
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/tweimann"><img src="./icons/github.png" class="socials-icon"></img> @tweimann</a><br>\
-        <a target="_blank" rel="noopener noreferrer" href="https://gitlab.com/tweimann"><img src="./icons/gitlab.png" class="socials-icon"></img> @tweimann</a><br>\
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/tweimann"><img src="https://content.tweimann.xyz/icons/github.png" class="socials-icon"></img> @tweimann</a><br>\
+        <a target="_blank" rel="noopener noreferrer" href="https://gitlab.com/tweimann"><img src="https://content.tweimann.xyz/icons/gitlab.png" class="socials-icon"></img> @tweimann</a><br>\
     </p>\
     <p class="watermark">\
             <a target="_blank" rel="noopener noreferrer" href="https://github.com/tweimann">tweimann</a> 2021 \u2022 Made with \
