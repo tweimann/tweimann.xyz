dc4022ea35090197579fb7a34bbf9c521b227239
tweimann <t@tweimann.xyz>
2021-12-13 21:34:24 +0100

    changed heart link in footer script to always get the right repo (probably last working commit for a while lmao)

--- js/footer.js
+++ js/footer.js
@@ -10,7 +10,7 @@ document.write('\
     </p>\
     <p class="watermark">\
             <a target="_blank" rel="noopener noreferrer" href="https://github.com/tweimann">tweimann</a> 2021 \u2022 Made with \
-            <a target="_blank" rel="noopener noreferrer" class="heart" href="https://github.com/tweimann/tweimann.xyz">\u2764</a> in Germany \u2022 \
+            <a target="_blank" rel="noopener noreferrer" class="heart" href="https://github.com/tweimann/' + window.location.hostname + '/">\u2764</a> in Germany \u2022 \
             <a href="imprint.html">Imprint</a>\
     </p>\
 </footer>');
